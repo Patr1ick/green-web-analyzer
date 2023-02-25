c5be8c2a7d52da214640476f3508518f16aaebab
Patr1ick <padi.geiger@gmail.com>
2023-02-25 17:30:22 +0100

    Fixed image source

--- frontend/src/components/Layout/TheHeader.vue
+++ frontend/src/components/Layout/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="flex flex-col md:flex-row justify-between content-center px-8 py-4 bg-emerald-800 dark:bg-zinc-900">
         <RouterLink to="/" class="flex flex-col md:flex-row items-center">
-            <img src="green-web-analyzer.svg" class="m-2 w-12" />
+            <img src="/green-web-analyzer.svg" class="m-2 w-12" />
             <h1 class="text-2xl text-gray-100 m-2 text-center hidden lg:block">Green Web Analyzer</h1>
         </RouterLink>
         <div class="flex justify-between content-center flex-col md:flex-row flex-wrap p-4 m-auto md:ml-auto md:mr-0">
