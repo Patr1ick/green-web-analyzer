9f5163cbb93312a74adfbad52fb8f185c82a5721
Patr1ick <padi.geiger@gmail.com>
2023-03-07 11:33:59 +0100

    Adjusted frontend to previous changes

--- frontend/src/components/Results/Criterias/CriteriaMinify.vue
+++ frontend/src/components/Results/Criterias/CriteriaMinify.vue
@@ -6,75 +6,47 @@
             </p>
         </template>
         <template #details>
-            <div class="overflow-x-auto">
-                <h2 class="font-bold">JavaScript files</h2>
-                <p v-if="result.details.files.js.length === 0">
-                    All files are minified.
-                </p>
-                <table v-else class="w-full p-4 border-separate border-spacing-1 ">
-                    <thead class="dark:bg-opacity-25 bg-gray-800">
-                        <tr>
-                            <th class="border border-gray-600 px-2">URL</th>
-                            <th class="border border-gray-600 px-2">Actual Size</th>
-                            <th class="border border-gray-600 px-2">Minified Size</th>
+            <section class="overflow-x-auto">
+                <div v-for="key in ['html', 'css', 'js']" :key="key">
+                    <h2 class="font-bold"><span class="uppercase">{{ key }}</span> files</h2>
+                    {{ }}
+                    <p v-if="result.details[key].files.length == 0">
+                        Either all files are minified or there are no files of this data type.
+                    </p>
+                    <table v-else class="w-full p-4 border-separate border-spacing-1 ">
+                        <thead class="dark:bg-opacity-25 bg-gray-800">
+                            <tr>
+                                <th class="border border-gray-600 px-2">URL</th>
+                                <th class="border border-gray-600 px-2">Actual Size</th>
+                                <th class="border border-gray-600 px-2">Minified Size</th>
+                            </tr>
+                        </thead>
+                        <tr v-for="file in result.details[key].files" class="text-gray-800 dark:text-gray-200">
+                            <td class="border border-gray-700 dark:border-gray-100 dark:border-opacity-25 break-all">
+                                <a :href="file.url" target="_blank" class="hover:underline">
+                                    {{ file.url }}
+                                </a>
+                            </td>
+                            <td class="border border-gray-700 dark:border-gray-100 dark:border-opacity-25">
+                                {{ convert(file.size) }}
+                            </td>
+                            <td class="border border-gray-700 dark:border-gray-100 dark:border-opacity-25">
+                                {{ convert(file.minified_size) }}
+                            </td>
                         </tr>
-                    </thead>
-                    <tr v-for="file in result.details.files.js" class="text-gray-800 dark:text-gray-200">
-                        <td class="border border-gray-700 dark:border-gray-100 dark:border-opacity-25 break-all">
-                            <a :href="file.url" target="_blank" class="hover:underline">
-                                {{ file.url }}
-                            </a>
-                        </td>
-                        <td class="border border-gray-700 dark:border-gray-100 dark:border-opacity-25">
-                            {{ convert(file.actual_size) }}
-                        </td>
-                        <td class="border border-gray-700 dark:border-gray-100 dark:border-opacity-25">
-                            {{ convert(file.minified_size) }}
-                        </td>
-                    </tr>
-                    <tr class="dark:bg-opacity-25 bg-gray-800">
-                        <td class="border border-gray-600 px-2">
-                            Summary
-                        </td>
-                        <td class="border border-gray-600 px-2">{{ convert(result.details.actual_size_js) }}</td>
-                        <td class="border border-gray-600 px-2">
-                            ??? (Saving {{ convert(result.details.potential_savings_js) }})
-                        </td>
-                    </tr>
-                </table>
-                <h2 class="font-bold">CSS files</h2>
-                <table class="w-full p-4 border-separate border-spacing-1 ">
-                    <thead class="dark:bg-opacity-25 bg-gray-800">
-                        <tr>
-                            <th class="border border-gray-600 px-2">URL</th>
-                            <th class="border border-gray-600 px-2">Actual Size</th>
-                            <th class="border border-gray-600 px-2">Minified Size</th>
+                        <tr class="dark:bg-opacity-25 bg-gray-800">
+                            <td class="border border-gray-600 px-2">
+                                Summary
+                            </td>
+                            <td class="border border-gray-600 px-2">{{ convert(result.details[key].total_size) }}</td>
+                            <td class="border border-gray-600 px-2">
+                                {{ convert(result.details[key].total_minified_size) }} (Saved {{
+                                    convert(result.details[key].total_savings) }})
+                            </td>
                         </tr>
-                    </thead>
-                    <tr v-for="file in result.details.files.css" class="text-gray-800 dark:text-gray-200">
-                        <td class="border border-gray-700 dark:border-gray-100 dark:border-opacity-25 break-all">
-                            <a :href="file.url" target="_blank" class="hover:underline">
-                                {{ file.url }}
-                            </a>
-                        </td>
-                        <td class="border border-gray-700 dark:border-gray-100 dark:border-opacity-25">
-                            {{ convert(file.actual_size) }}
-                        </td>
-                        <td class="border border-gray-700 dark:border-gray-100 dark:border-opacity-25">
-                            {{ convert(file.minified_size) }}
-                        </td>
-                    </tr>
-                    <tr class="dark:bg-opacity-25 bg-gray-800">
-                        <td class="border border-gray-600 px-2">
-                            Summary
-                        </td>
-                        <td class="border border-gray-600 px-2">{{ convert(result.details.actual_size_css) }}</td>
-                        <td class="border border-gray-600 px-2">
-                            ??? (Saving {{ convert(result.details.potential_savings_css) }})
-                        </td>
-                    </tr>
-                </table>
-            </div>
+                    </table>
+                </div>
+            </section>
         </template>
     </BasicAccordion>
 </template>
